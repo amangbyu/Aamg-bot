b3bfe13113a3b03ce0ddc47d9ef55c94c5142719
amangbyu <armandguanteng@gmail.com>
2026-03-27 08:46:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,34 +4,34 @@
 
 module.exports = {
     // Nomor WhatsApp bot (tanpa +)
-    ownerNumber: "6283825530372",
+    ownerNumber: "6282312622511",
 
     // Owner bot (bisa lebih dari satu)
-    ownerList: ["6283825530372"],
+    ownerList: ["6282312622511"],
 
     // Prefix command
-    prefix: ".",
+    prefix: "0-",
 
     // Nama bot
-    botName: "AMG Bot",
+    botName: "Play Bot",
 
     // Mode self: true = hanya owner yang bisa pakai
     self: false,
 
     // ======= CHANNEL WA =======
     // ID channel WA (format: 120363xxxxxxxxxx@newsletter)
-    channelId: "120363407084338458@newsletter",
+    channelId: "120363406845501686@newsletter",
 
     // Link channel WA ( buat playch)
-    channelLink: "https://whatsapp.com/channel/0029VbBoUi18qIzvxswunm00",
+    channelLink: "https://whatsapp.com/channel/0029VbCCIGUHgZWblWQi6L0M",
 
     // ======= MEDIA =======
     // URL thumbnail default (foto profil fallback)
-    thumb: "https://files.catbox.moe/1chxd9.jpeg",
+    thumb: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwhVUwgUuyo5mp1V5DPdU0srsRrmeXi83d-Q&s",
 
     // ======= API =======
     // API key BotcahX (untuk download YouTube)
-    apiKey: "__",
+    apiKey: "AmgPro",
     // URL API botcahx/fallback
     apiUrl: "https://api.botcahx.eu.org",
 };
